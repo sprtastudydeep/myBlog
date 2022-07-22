85337fc171ccadc098ce07cb5cf586b7264fa791
cesdea <aprkvnem@gmail.com>
2022-07-22 22:09:37 +0900

    add ignore

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connect = require("./schemas");
+connect();
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(port, '포트로 서버가 열렸어요!');
+});
+
+app.use(express.json());
+
+
+app.use("/api", [goodsRouter])
