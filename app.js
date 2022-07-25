aa0ea57d9314a5962adba959f149fed8d6681699
cesdea <aprkvnem@gmail.com>
2022-07-25 16:26:47 +0900

    add rollback

--- app.js
+++ app.js
@@ -22,4 +22,4 @@ app.use(express.json());
 const postsRouter = require("./routes/posts");
 const commentRouter = require("./routes/comment");
 
-app.use("/api/posts", [postsRouter,commentRouter]);
+app.use("/posts", [postsRouter,commentRouter]);
