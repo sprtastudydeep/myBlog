9dd15c76eeeae2a22e693d02b8a0d0ac512fbc2e
cesdea <aprkvnem@gmail.com>
2022-07-30 17:42:04 +0900

    delete schemas,add models(require)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const connect = require("./schemas");
+const { User } = require("./models");
 connect();
 const app = express();
 const port = 8080;
