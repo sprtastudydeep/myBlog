ed7f4170d540cb6cf0810895ac03dfb54df87f22
cesdea <aprkvnem@gmail.com>
2022-08-02 22:16:10 +0900

    create post 존재확인 모듈화

---
+++ middlewares/existBoard.js
@@ -0,0 +1,18 @@
+
+const { Post } = require("../models");
+
+module.exports = async(req, res, next) => {
+    const {postId} = req.params;
+    const exists = await Post.findAll({
+        where:{'postId':postId},
+        limit:1
+    })
+    if (exists.length==0) {
+    res.status(400).send({
+        errorMessage: "NONE_EXIST_BOARD",
+    });
+    return;
+    }
+    res.locals.exists=exists[0].dataValues
+    next()
+}
