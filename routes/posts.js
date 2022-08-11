748a53774051a25adbff5b442b7d2f962fb83365
subeomlee <dltnqja9@naver.com>
2022-08-11 16:34:03 +0900

    이름바꾸기

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const loginMiddleware = require('../middlewares/login-middleware');
+const authmiddleware = require('../middlewares/auth-middleware');
 
 const PostsController = require('../controllers/posts.controller');
 const postsController = new PostsController();
 
 router.get('/', postsController.getPosts);
 router.get('/:postId', postsController.getPostById);
-router.post('/', loginMiddleware, postsController.createPost);
-router.put('/:postId', loginMiddleware, postsController.updatePost);
-router.delete('/:postId', loginMiddleware, postsController.deletePost);
+router.post('/', authmiddleware, postsController.createPost);
+router.put('/:postId', authmiddleware, postsController.updatePost);
+router.delete('/:postId', authmiddleware, postsController.deletePost);
 
 module.exports = router;
