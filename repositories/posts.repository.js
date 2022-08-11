0b93f008f94822b4b7848ebbbf0d5f09f47af6e3
SilverTree <81796548+cesdea@users.noreply.github.com>
2022-08-11 16:40:50 +0900

    Create posts.repository.js

---
+++ repositories/posts.repository.js
@@ -0,0 +1,36 @@
+const { Post } = require('../models');
+
+class PostRepository {
+  findAllPost = async () => {
+    const posts = await Post.findAll();
+
+    return posts;
+  };
+  findPostById = async (postId) => {
+    const post = await Post.findByPk(postId);
+
+    return post;
+  };
+  createPost = async (postName, postTitle, postContent) => {
+    const createdPost = await Post.create({
+      postName,
+      postTitle,
+      postContent,
+    });
+    return createdPost;
+  };
+  updatePost = async (postId, postTitle, postContent) => {
+    const updatePost = await Post.update(
+      { postTitle, postContent },
+      { where: { postId } }
+    );
+
+    return updatePost;
+  };
+  deletePost = async (postId) => {
+    const deletePost = await Post.destroy({ where: { postId } });
+    return deletePost;
+  };
+}
+
+module.exports = PostRepository;
