9f168074754f83adaa8e7d831d739c26d2de58be
soularofdawn <106153814+soularofdawn@users.noreply.github.com>
2022-08-11 19:40:29 +0900

    like 기능 구현

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -1,8 +1,8 @@
-const { Post } = require('../models');
+const { Post,Like } = require('../models');
 
 class PostRepository {
   findAllPost = async () => {
-    const posts = await Post.findAll();
+    const posts = await Post.findAll({order:[['createdAt','desc']]});
 
     return posts;
   };
@@ -31,6 +31,27 @@ class PostRepository {
     const deletePost = await Post.destroy({ where: { postId } });
     return deletePost;
   };
+  
+  findLikeList = async(userId) => {
+    const posts = await Like.findAll({where: {userId}})
+    return posts
+  }
+  findLikesNum = async(postId) => {
+    const like = await Like.count({where:{postId}})
+    return like
+  }
+  findLike = async(postId,userId) => {
+    const post = await Like.findOne({where: {userId,postId}})
+    return post
+  }
+  deleteLike = async(id) => {
+    await Like.destroy({where: {id}})
+    return
+  }
+  createLike = async(postId, userId) => {
+    await Like.create({postId,userId})
+    return
+  }
 }
 
 module.exports = PostRepository;
