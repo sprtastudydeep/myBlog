c4533ada5430eb557e496fa5b60b6a5152747ce6
soularofdawn <106153814+soularofdawn@users.noreply.github.com>
2022-08-11 19:39:06 +0900

    like 기능 구현

--- services/posts.service.js
+++ services/posts.service.js
@@ -6,19 +6,21 @@ class PostService {
   findAllPost = async () => {
     const allPost = await this.postRepository.findAllPost();
 
-    allPost.sort((a, b) => {
-      return b.createdAt - a.createdAt;
-    });
-
-    return allPost.map((post) => {
-      return {
+    let posts = []
+    for(let i in allPost){
+      const post = allPost[i].dataValues;
+      const likes = await this.postRepository.findLikesNum(post.postId)
+      const postInfo = {
         postId: post.postId,
         postName: post.postName,
         postTitle: post.postTitle,
         createdAt: post.createdAt,
         updatedAt: post.updatedAt,
+        likes
       };
-    });
+      posts.push(postInfo)
+    }
+    return posts
   };
   findPostById = async (postId) => {
     const findPost = await this.postRepository.findPostById(postId);
@@ -64,6 +66,41 @@ class PostService {
     }
     const postdelete = await this.postRepository.deletePost(postId);
     return { postdelete };
+  };
+  
+  likePost = async (postId, userId) => {
+    const post = await this.postRepository.findPostById(postId);
+    if (!post) {
+      return { status: 400, messege: "Invalid postId" };
+    }
+
+    const existLike = await this.postRepository.findLike(postId, userId);
+    if (existLike) {
+      await this.postRepository.deleteLike(existLike.id);
+      return { status: 201, messege: "좋아요 취소" };
+    } else {
+      await this.postRepository.createLike(postId, userId);
+      return { status: 201, messege: "좋아요 성공" };
+    }
+  };
+
+  likePostList = async (userId) => {
+    const likePostList = await this.postRepository.findLikeList(userId);
+
+    const posts = likePostList.map(async (p) => {
+      const post = await this.postRepository.findPostById(p.postId);
+      const likes = await this.postRepository.findLikesNum(p.postId);
+      return {
+        postId: post.postId,
+        postTitle: post.postTitle,
+        postName: post.postName,
+        likes,
+      };
+    });
+    const sortingPosts = posts.sort((a, b) => {
+      return b.likes - a.likes;
+    });
+    return { status: 200, data: sortingPosts };
   };
 }
 
